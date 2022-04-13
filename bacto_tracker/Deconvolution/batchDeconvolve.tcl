98b0330b95dbbc0e1e14f762e7ba568cdac95dc0
marcnol <marcnol@gmail.com>
2022-04-13 09:50:32 +0200

    removed deconvolution scripts and moved networks

--- bacto_tracker/Deconvolution/batchDeconvolve.tcl
+++
@@ -1,9 +0,0 @@
-# batchDeconvolve.tcl
-set tcl_files "/home/marcnol/Repositories/huygensDeconvolutionScripts/"
-
-set dataDir "/mnt/grey/DATA/rawData_2020/Experiment_5/DAPI/005_merFISH_RAMM_DAPI"
-set destDir "/home/marcnol/tmp/"
-
-source "${tcl_files}merfish.tcl"
-
-decon_segment "$tcl_files" "$dataDir" "$destDir"
